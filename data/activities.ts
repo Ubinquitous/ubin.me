505497caa78e88fac7034e26a1252a5d2a32cb4c
Ubinquitous <ubinquitous1@gmail.com>
2023-07-13 16:16:21 +0900

    chore(data): activity data 추가

---
+++ data/activities.ts
@@ -0,0 +1,61 @@
+import Activity1 from '/assets/activity/activity01.jpeg';
+import Activity2 from '/assets/activity/activity02.jpeg';
+import Activity3 from '/assets/activity/activity03.jpeg';
+import Activity4 from '/assets/activity/activity04.jpeg';
+import Activity5 from '/assets/activity/activity05.jpeg';
+import Activity6 from '/assets/activity/activity06.jpeg';
+import Activity7 from '/assets/activity/activity07.jpeg';
+import Activity8 from '/assets/activity/activity08.jpeg';
+import Activity9 from '/assets/activity/activity09.jpeg';
+import Activity10 from '/assets/activity/activity10.jpeg';
+import Activity11 from '/assets/activity/activity11.jpeg';
+import Activity12 from '/assets/activity/activity12.jpeg';
+
+const events = [
+  { content: 'Microsoft Azure 자격증 취득', date: '2022-05-26' },
+  { content: '2022 직업계고 V-LOG 공모전 최우수상', date: '2022-05-29' },
+  { content: '2022 SKT Smarteen App Challenge 대회 참여', date: '2022-06-12' },
+  { content: '2022 BSSM 1학기 해커톤 참가', date: '2022-07-13' },
+  { content: '2022 1학기 교과우수상 ( 디자인 일반 )', date: '2022-07-15' },
+  { content: '2022 교내 포트폴리오 경진 대회 최우수상', date: '2022-08-01' },
+  { content: '2022 SW마이스터고 연합 토크콘서트 참가', date: '2022-08-21' },
+  { content: '2022 BSSM 알고리즘 코딩 테스트 대회 대상(1위)', date: '2022-08-26' },
+  { content: '2022 교내 3차 입학 설명회 학생 대표로 발표', date: '2022-08-27' },
+  { content: '2022 교내 4차 입학 설명회 학생 대표로 발표', date: '2022-09-14' },
+  { content: '2022 nypc 예선 통과', date: '2022-09-14' },
+  { content: '2022 교내 5차 입학 설명회 학생 대표로 발표', date: '2022-10-05' },
+  { content: '2022 교내 진로 탐색 대회 장려상(3위)', date: '2022-10-25' },
+  { content: '2022 Devfest GDG Daegu 해커톤 참여', date: '2022-12-02' },
+  { content: '2022 BDIA JOB FAIR 벡스코 참여', date: '2022-12-08' },
+  { content: '2022 BDIA JOB FAIR 알고리즘 대회 참여', date: '2022-12-09' },
+  { content: '2022 BSSM 2학기 교내 AI캠프 참가', date: '2022-12-30' },
+  { content: '2022 BSSM 2학기 교내 데이터톤 참가', date: '2023-01-13' },
+  { content: "2022 교내 동아리 'T자형 인재' 1학년 팀장", date: '2022-08-01 ~ 2023-02~28' },
+  { content: '2022 2학기 교과우수상 ( 통합사회, 음악, 컴퓨터 구조 )', date: '2023-01-05' },
+  { content: '2022 전국 직업계고 창업캠프 최우수상 ', date: '2023-02-11' },
+  { content: "2023 교내 동아리 'INSERT' 2학년 팀장", date: '2023-03-01 ~' },
+  { content: '정보처리산업기사 자격증 취득', date: '2023-05-26' },
+  { content: "2023 사설 동아리 'WOONGSERT' 프론트엔드 리더", date: '2023-06 ~' },
+  { content: '2023 INSERT 인턴 프론트엔드 멘토', date: '2023-05-14 ~ 2023-07-14' },
+  { content: '2023 교내 1차 입학 설명회 학생 대표로 발표', date: '2023-07-08' },
+  { content: '2023 교내 AI캠프 해커톤 최우수상', date: '2023-07-07' },
+  { content: '2023 교내 산학협력프로젝트 경진대회 대상', date: '2023-07-08' },
+  { content: '2023 부산 직업박람회 학교 대표로 참여', date: '2023-07-12' }
+];
+
+const assets = [
+  Activity1,
+  Activity2,
+  Activity3,
+  Activity4,
+  Activity5,
+  Activity6,
+  Activity7,
+  Activity8,
+  Activity9,
+  Activity10,
+  Activity11,
+  Activity12
+];
+
+export { events, assets };
